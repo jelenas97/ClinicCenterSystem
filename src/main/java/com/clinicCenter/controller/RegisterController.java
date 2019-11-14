ce176249f391399444c4d390bd95014ba586f3d4
jelenas97 <jekibp@gmail.com>
2019-11-14 22:12:20 +0100

    Something

--- src/main/java/com/clinicCenter/controller/RegisterController.java
+++ src/main/java/com/clinicCenter/controller/RegisterController.java
@@ -2,15 +2,21 @@ package com.clinicCenter.controller;
 
 import com.clinicCenter.model.Medicament;
 import com.clinicCenter.model.Patient;
+import com.clinicCenter.model.RegistrationRequest;
 import com.clinicCenter.model.User;
+import com.clinicCenter.service.RegistrationRequestService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @CrossOrigin(origins = "http://localhost:4200")
 public class RegisterController {
 
+    @Autowired
+    private RegistrationRequestService registrationRequestService;
+
     @PostMapping("/registration")
-    public void getUser(@RequestBody Medicament medicament){
-        System.out.println("USO");
+    public void getUser(@RequestBody RegistrationRequest registrationRequest){
+      registrationRequestService.save(registrationRequest);
     }
 }
