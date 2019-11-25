97c4d7988ba07fc118cc91264fe5666c227ec92b
jelenas97 <jekibp@gmail.com>
2019-11-25 20:31:35 +0100

    Add ClinicAdministratorDto

---
+++ src/main/java/com/clinicCenter/dto/ClinicAdministratorDto.java
@@ -0,0 +1,14 @@
+package com.clinicCenter.dto;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class ClinicAdministratorDto {
+
+    private String id;
+    private String firstName;
+    private String lastName;
+}
