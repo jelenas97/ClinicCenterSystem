7fb583659e393bb52cc4e337a40d180418c549a2
jelenas97 <jekibp@gmail.com>
2019-11-25 12:59:52 +0100

    Queries for getting all clinical admins from db

--- src/main/java/com/clinicCenter/controller/ClinicController.java
+++ src/main/java/com/clinicCenter/controller/ClinicController.java
@@ -2,11 +2,9 @@ package com.clinicCenter.controller;
 
 import com.clinicCenter.model.Clinic;
 import com.clinicCenter.service.ClinicService;
+import com.clinicCenter.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @CrossOrigin(origins = "http://localhost:4200")
@@ -19,4 +17,5 @@ public class ClinicController {
     public void saveClinic(@RequestBody Clinic clinic){
         clinicService.save(clinic);
     }
+
 }
--- src/main/java/com/clinicCenter/service/UserService.java
+++ src/main/java/com/clinicCenter/service/UserService.java
@@ -1,12 +1,16 @@
 package com.clinicCenter.service;
 
+import com.clinicCenter.model.ClinicAdministrator;
 import com.clinicCenter.model.Patient;
 import com.clinicCenter.model.User;
 import com.clinicCenter.model.UserMapper;
+
+import java.util.Set;
 
 public interface UserService {
 
     User getById(Long id);
     int updateMedicalStaff(Long id, UserMapper user);
     void save(Patient patient);
+    Set<String> getAllAdmins();
 }
