93cf1132c00e20a93ca85aebbb296b9476ec9cdf
petarcurcin <petar.curcin97@gmail.com>
2019-12-11 23:06:28 +0100

    create controler service and repository

---
+++ src/main/java/com/clinicCenter/service/MedicalExaminationTypeService.java
@@ -0,0 +1,9 @@
+package com.clinicCenter.service;
+
+import com.clinicCenter.model.MedicalExaminationType;
+
+import java.util.Set;
+
+public interface MedicalExaminationTypeService {
+    Set<MedicalExaminationType> getAll();
+}
