9d6db84464f5a9db8bb74480ffcf0df4cf0a7581
petarcurcin <petar.curcin97@gmail.com>
2019-12-13 12:29:54 +0100

    quick changes

--- src/main/java/com/clinicCenter/controller/MedicalExaminationTypeController.java
+++ src/main/java/com/clinicCenter/controller/MedicalExaminationTypeController.java
@@ -3,6 +3,7 @@ package com.clinicCenter.controller;
 import com.clinicCenter.model.MedicalExaminationType;
 import com.clinicCenter.service.MedicalExaminationTypeService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
--- src/main/java/com/clinicCenter/service/implementation/MedicalExaminationTypeServiceImpl.java
+++ src/main/java/com/clinicCenter/service/implementation/MedicalExaminationTypeServiceImpl.java
@@ -8,6 +8,7 @@ import com.clinicCenter.service.MedicalExaminationTypeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Collection;
 import java.util.Set;
 
 @Service
@@ -34,6 +35,11 @@ public class MedicalExaminationTypeServiceImpl implements MedicalExaminationType
     @Override
     public int updateType(MedicalExaminationType type) {
         return this.medicalExaminationTypeRepository.updateType(type.getId(), type.getName(), type.getPrice());
+    }
+
+    @Override
+    public Collection<MedicalExaminationType> getAllMedicalExaminationTypes() {
+        return medicalExaminationTypeRepository.findAll();
     }
 
     @Override
